0b99f547c7e7ba986b7ab4c1742a7162721e45b8
amiraelsayedbadwy <amiraelsayed62@hotmail.com>
2021-12-24 18:28:42 +0300

    * .gitignore: custom font in ios
    * TabbedPageIos.sln:
    * App.xaml:
    * MyPage.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MyPage3.xaml:
    * MyPage2.xaml:
    * MainPage.xaml:
    * MyPage.xaml.cs:
    * Info.plist:
    * AssemblyInfo.cs:
    * MyPage2.xaml.cs:
    * MyPage3.xaml.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * TabbedPageIos.csproj:
    * buy.png:
    * Entitlements.plist:
    * MainActivity.cs:
    * search.png:
    * buy@2x.png:
    * buy@3x.png:
    * listing.png:
    * Default.png:
    * settings.png:
    * search@3x.png:
    * search@2x.png:
    * listing@2x.png:
    * Default@2x.png:
    * TabbedPageIos.iOS.csproj:
    * listing@3x.png:
    * settings@2x.png:
    * settings@3x.png:
    * Lato-Regular.ttf:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * ExtendedTabbedPageRenderer.cs:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * TabbedPageIos.Android.csproj:
    * AboutResources.txt:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon60.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon87.png:
    * Icon76.png:
    * Icon80.png:
    * Icon58.png:
    * icon_round.xml:
    * Icon167.png:
    * Icon120.png:
    * Icon152.png:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ TabbedPageIos.iOS/ExtendedTabbedPageRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TabbedPageIos.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(ExtendedTabbedPageRenderer))]
+namespace TabbedPageIos.iOS
+{
+    public class ExtendedTabbedPageRenderer : TabbedRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            if (TabBar?.Items == null)
+                return;
+
+            var tabs = Element as TabbedPage;
+            if (tabs != null)
+            {
+                for (int i = 0; i < TabBar.Items.Length; i++)
+                {
+                    UpdateTabBarItem(TabBar.Items[i], tabs.Children[i].Icon);
+                }
+            }
+
+            base.ViewWillAppear(animated);
+        }
+
+        private void UpdateTabBarItem(UITabBarItem item, string icon)
+        {
+            if (item == null || icon == null)
+                return;
+
+            // Set the font for the title.
+            item.SetTitleTextAttributes(new UITextAttributes() { Font = UIFont.FromName("GillSans-SemiBold", 18), TextColor = Color.FromHex("#3C9BDF").ToUIColor() }, UIControlState.Selected);
+
+            item.SetTitleTextAttributes(new UITextAttributes() { Font = UIFont.FromName("GillSans-SemiBold", 14), TextColor = Color.FromHex("#757575").ToUIColor() }, UIControlState.Normal);
+        }
+    }
+}
---
+++ TabbedPageIos/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbedPageIos
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SelectedTabColor = Color.FromHex("#15544C");
+            UnselectedTabColor = Color.FromHex("#989b9a");
+
+            NavigationPage shackBuyPage = new NavigationPage(new MyPage());
+            shackBuyPage.IconImageSource = "buy.png";
+            shackBuyPage.Title = "ShackBuy";
+            shackBuyPage.BarTextColor = Color.FromHex("#15544C");
+
+            NavigationPage buyPage = new NavigationPage(new MyPage2());
+            buyPage.IconImageSource = "search.png";
+            buyPage.Title = "Search";
+            buyPage.BarTextColor = Color.FromHex("#15544C");
+            NavigationPage listingPage = new NavigationPage(new MyPage3());
+            listingPage.IconImageSource = "listing.png";
+            listingPage.Title = "Sell";
+            listingPage.BarTextColor = Color.FromHex("#15544C");
+            NavigationPage settingsPage = new NavigationPage(new MyPage());
+            settingsPage.IconImageSource = "settings.png";
+            settingsPage.Title = "Account";
+            settingsPage.BarTextColor = Color.FromHex("#15544C");
+            Children.Add(shackBuyPage);
+            Children.Add(buyPage);
+            Children.Add(listingPage);
+            Children.Add(settingsPage);
+
+        }
+    }
+}
